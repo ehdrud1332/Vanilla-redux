4ec8cf0317c28862f22b16ae4ee26ba3b863b346
castle.jaemin <ehdrud1332@gmail.com>
2021-04-03 14:41:47 +0900

    4.5 createSlice
    reducer & action을 둘다 담고 있는 toolkit 함수

--- src/store.js
+++ src/store.js
@@ -1,47 +1,19 @@
-import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// 리덕스 toolkit을 이용한 코드 간결화 createAction안에 타입의 인수를 넣는다.
-// createAction은 항상 type과 payload를 반환한다.
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-// const addToDo = (text) => {
-//   return {
-//     type: ADD,
-//     text,
-//   };
-// };
-//
-// const deleteToDo = (id) => {
-//   return {
-//     type: DELETE,
-//     id: parseInt(id),
-//   };
-// };
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter((toDo) => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: toDos.reducer });
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+export const { add, remove } = toDos.actions;
 
 export default store;
